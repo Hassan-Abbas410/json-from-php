dcbcbb786187bde20aa217bcf57b685656bdbd09
Hassan-Abbas410 <202392410@student.ttcollege.edu.sa>
2019-11-28 01:29:54 +0300

    H

--- app/src/main/java/com/example/json_from_php/MainActivity.java
+++ app/src/main/java/com/example/json_from_php/MainActivity.java
@@ -1,23 +1,29 @@
 package com.example.json_from_php;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
-    Button bttn_sqlConn;
+    Button bttn_sqlConn,ButtonView;
     TextView textview;
+
     phpConn phpC = new phpConn();
-    final String fetch = "http://192.168.11.141:8080/sqli/fetch2json.php";
+    final String fetch = "http://192.168.1.7/sqli/fetch2json.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         textview=(TextView)findViewById(R.id.textView);
         bttn_sqlConn= (Button)findViewById(R.id.bttnSQLCon);
@@ -32,13 +38,16 @@ public class MainActivity extends AppCompatActivity {
 
                     /* reading the JSON array line by line */
                     for (int i = 0; i < js_array.length(); i++) {
-                        String value=js_array.getString(i);
+                        String value = js_array.getString(i);
                         Log.v("george json", i+"="+value);
                     }
                 } catch (Exception e) {
                     Log.e("Error", e.toString());
                 }
+
             }
         });
+
+
     }
 }
